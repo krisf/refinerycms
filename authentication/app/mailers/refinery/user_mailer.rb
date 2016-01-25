800b9d371c82c666628b690de771ad265ee2c7ea
krisf <kris@gohosted.ca>
2016-01-25 12:06:38 -0500

    Update user_mailer.rb

--- authentication/app/mailers/refinery/user_mailer.rb
+++ authentication/app/mailers/refinery/user_mailer.rb
@@ -10,7 +10,7 @@ module Refinery
 
       mail(:to => user.email,
            :subject => t('subject', :scope => 'refinery.user_mailer.reset_notification'),
-           :from => "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>")
+           :from => "\"#{Refinery::Core.site_name}\" <recruiter-id-app@forces.ca>")
     end
 
   protected
